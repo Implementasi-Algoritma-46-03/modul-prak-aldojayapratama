037fa1e042f544472d6acfd95c5244965458680c
aldojayapratama <aldojayapratama3@gmail.com>
2025-09-29 18:33:15 +0700

    Training TP

--- Modul03/TP01.java
+++ Modul03/TP01.java
@@ -1,6 +1,30 @@
-public class TP01 {
+ public class TP01 {
+	public static void main(final String[] args) {
+		System.out.println("----------");
+		System.out.println("Tabungan Dira");
+		System.out.println("Masukan 1");
+		System.out.println("==========");
+		int X = 500000;
+		int Y = 10000;
+		int N = 30;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
-    }
+		
+		int total = X + (Y*N);
+		System.out.println(total);
+
+
+		System.out.println("==========");
+		System.out.println("Tabungan Dira");
+		System.out.println("Masukan 2");
+		System.out.println("----------");
+		int X2 = 250000;
+		int Y2 = 25000;
+		int N2 = 30;
+
+		
+		int total2 = X2 + (Y2*N2);
+		System.out.println(total2);
+	}
 }
+    
+
--- Modul03/TP03.java
+++ Modul03/TP03.java
@@ -1,6 +1,28 @@
 public class TP03 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        int JumlahPermen = 12;
+        int JumlahTemanDira = 3;
+
+        int TotalOrang = JumlahTemanDira + 1; // plus dira
+
+        int Jatah = JumlahPermen / TotalOrang;
+        int Sisa = JumlahPermen % TotalOrang;
+
+        System.out.println("==Masukan #1==");
+        System.out.println("Jumlah Permen Masing-Masing : " + Jatah);
+        System.out.println("Sisa Permen Dira : " + Sisa);
+        System.out.println();
+
+        int JumlahPermen2 = 20;
+        int JumlahTemanDira2 = 2;
+
+        int TotalOrang2 = JumlahTemanDira2 + 1; // plus dira
+
+        int Jatah2 = JumlahPermen2 / TotalOrang2;
+        int Sisa2 = JumlahPermen2 % TotalOrang2;
+        System.out.println("==Masukan #2==");
+        System.out.println("Jumlah Permen Masing-Masing : " + Jatah2);
+        System.out.println("Sisa Permen Dira : " +Sisa2);
     }
 }
