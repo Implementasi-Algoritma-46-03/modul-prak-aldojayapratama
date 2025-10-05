a1d6154787807be540f15e57e078c200a2434a31
aldojayapratama <aldojayapratama3@gmail.com>
2025-10-05 23:44:28 +0700

    end

--- Modul04/TP01.java
+++ Modul04/TP01.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class TP01 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
         int N = input.nextInt();
--- Modul04/TP02.java
+++ Modul04/TP02.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class TP02 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
         final double PERSENTASE_DISKON = 0.05;
