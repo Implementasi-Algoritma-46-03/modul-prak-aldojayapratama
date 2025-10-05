f8ad964d3e3e371796f8b018dc136a8cfef6d17d
aldojayapratama <aldojayapratama3@gmail.com>
2025-10-05 23:20:15 +0700

    Pengumpul Jurnal

--- Modul04/TP01.java
+++ Modul04/TP01.java
@@ -1,6 +1,18 @@
-public class TP01 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class TP01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int N = input.nextInt();
+
+        if (N % 6 == 0) {
+            System.out.println("Kelipatan enam");
+        } else {
+            System.out.println("Bukan kelipatan enam");
+        }
+
+        input.close();
     }
 }
+
--- Modul04/TP02.java
+++ Modul04/TP02.java
@@ -1,6 +1,30 @@
-public class TP02 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class TP02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        final double PERSENTASE_DISKON = 0.05;
+        final int MINIMAL_TRANSAKSI_DISKON = 200000;
+        final double PERSENTASE_PPN = 0.11;
+
+        int N = input.nextInt();
+
+        double jumlahDiskon = 0;
+
+        if (N > MINIMAL_TRANSAKSI_DISKON) {
+            jumlahDiskon = N * PERSENTASE_DISKON;
+        }
+
+        double totalSetelahDiskon = N - jumlahDiskon;
+        double pajakPpn = totalSetelahDiskon * PERSENTASE_PPN;
+        double totalDibayar = totalSetelahDiskon + pajakPpn;
+
+        System.out.println("Transaksi = " + N);
+        System.out.println("Total = " + totalSetelahDiskon);
+        System.out.println("Ppn 11% = " + pajakPpn);
+        System.out.println("Total dibayar = " + totalDibayar);
+
+        input.close();
     }
 }
