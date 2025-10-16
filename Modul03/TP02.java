4ddbbdda6a3f6447f1e0b8a499d3559a9cf157f7
aldojayapratama <aldojayapratama3@gmail.com>
2025-10-16 18:57:40 +0700

    Perbaikan Modul

--- Modul03/TP02.java
+++ Modul03/TP02.java
@@ -1,30 +1,12 @@
- public class TP01 {
-	public static void main(final String[] args) {
-		System.out.println("----------");
-		System.out.println("Tabungan Dira");
-		System.out.println("Masukan 1");
-		System.out.println("==========");
-		int X = 500000;
-		int Y = 10000;
-		int N = 30;
+public class TP02 {
 
-		
-		int total = X + (Y*N);
-		System.out.println(total);
+    public static void main(final String[] args) {
+        String Nama = "Dira";
 
+        int Apel = 2000;
+        int JumlahBarang = 5;
+        int TotalBayar = JumlahBarang * Apel; 
 
-		System.out.println("==========");
-		System.out.println("Tabungan Dira");
-		System.out.println("Masukan 2");
-		System.out.println("----------");
-		int X2 = 250000;
-		int Y2 = 25000;
-		int N2 = 30;
-
-		
-		int total2 = X2 + (Y2*N2);
-		System.out.println(total2);
-	}
+        System.out.println("Hi, " + Nama + ". Total belanja adalah " + TotalBayar + " rupiah.");
+    }
 }
-    
-
--- Modul05/Jurnal03.java
+++ Modul05/Jurnal03.java
@@ -1,6 +1,35 @@
+import java.util.Scanner;
+
 public class Jurnal03 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        char O = scanner.next().charAt(0);
+        int B = scanner.nextInt();
+
+        switch (O) { 
+            case '+':
+                System.out.println(A + B);
+                break;
+            case '-':
+                System.out.println(A - B);
+                break;
+            case '*':
+                System.out.println(A * B);
+                break;
+            case '/':
+                if (B != 0) {
+                    System.out.println((double) A / B);
+                } else {
+                    System.out.println("Pembagi tidak boleh 0");
+                }
+                break;
+            default:
+                System.out.println("Tanda operasi tidak valid");
+        }
     }
 }
