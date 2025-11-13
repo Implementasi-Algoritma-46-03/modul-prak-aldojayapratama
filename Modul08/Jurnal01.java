eb3afea300be17fe3ae770511b656fe7aabf1651
aldojayapratama <aldojayapratama3@gmail.com>
2025-11-13 17:24:43 +0700

    update

--- Modul08/Jurnal01.java
+++ Modul08/Jurnal01.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
-
 public class Jurnal01 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int r = scanner.nextInt();
-        int n = scanner.nextInt();
-        
-        double sisa = r * Math.pow(0.5, n / 10.0);
+
+    public static void main(final String[] args) {
+        Scanner input = new Scanner(System.in);
+        double R = input.nextDouble();
+        double N = input.nextDouble();
+
+        double sisa = R * Math.pow(0.5, N/ 10.0);
+
         System.out.printf("%.3f\n", sisa);
     }
 }
