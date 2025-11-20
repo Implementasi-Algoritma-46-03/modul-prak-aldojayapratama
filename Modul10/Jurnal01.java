d92872a7ebf10c2bad9189f8f0d822d5595842dc
aldojayapratama <aldojayapratama3@gmail.com>
2025-11-20 17:07:39 +0700

    Jurnal Modul10

--- Modul10/Jurnal01.java
+++ Modul10/Jurnal01.java
@@ -1,6 +1,22 @@
 public class Jurnal01 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 6; i++) {
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+            //untuk mencari barisan genap
+            if (i % 2 == 0) {
+                System.out.print(" "); 
+            }
+
+            for (int j = 1; j <= 5; j++) {
+                if (j == 5 && i % 2 == 0) {
+                    System.out.print(i);
+                }
+                else {
+                    System.out.print(i + " ");
+                }
+            }
+
+            System.out.println(); 
+        }
     }
 }
--- Modul10/Jurnal03.java
+++ Modul10/Jurnal03.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Jurnal03 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner sc= new Scanner(System.in);
+        int ukuranPola = sc.nextInt();
+        
+        int angkaSekarang = 0;
+        
+        for (int baris = 1; baris <= ukuranPola; baris++) {
+            int jumlahAngka = ukuranPola - baris + 1;
+            
+            for (int kolom = 1; kolom <= jumlahAngka; kolom++) {
+                System.out.print(angkaSekarang % 10);
+                angkaSekarang++;
+            }
+            System.out.println();
+        }
+        
+        sc.close();
     }
 }
