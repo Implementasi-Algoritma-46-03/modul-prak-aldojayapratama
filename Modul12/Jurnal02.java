096da64795a8c1be7b631c725b225c47aa7cae31
aldojayapratama <aldojayapratama3@gmail.com>
2025-12-04 17:06:37 +0700

    update Modul12

--- Modul12/Jurnal02.java
+++ Modul12/Jurnal02.java
@@ -1,56 +1,91 @@
 import java.util.Scanner;
 
 public class Jurnal02 {
-
-    public static char checkWinner(int[][] board, int N) {
-        boolean hasEmptyCell = false;
-
-        for (int i = 0; i < N; i++) {
-            int rowO = 0, rowX = 0, colO = 0, colX = 0;
-
-            for (int j = 0; j < N; j++) {
-                if (board[i][j] == 0) {
-                    hasEmptyCell = true;
-                }
-                if (board[i][j] == 1) rowO++;
-                if (board[i][j] == 2) rowX++;
-                if (board[j][i] == 1) colO++;
-                if (board[j][i] == 2) colX++;
-            }
-            if (rowO == N || colO == N) return 'O';
-            if (rowX == N || colX == N) return 'X';
-        }
-        int leftDiagonalO = 0, leftDiagonalX = 0;
-        int rightDiagonalO = 0, rightDiagonalX = 0;
-
-        for (int i = 0; i < N; i++) {
-            if (board[i][i] == 1) leftDiagonalO++;
-            if (board[i][i] == 2) leftDiagonalX++;
-            if (board[i][N - 1 - i] == 1) rightDiagonalO++;
-            if (board[i][N - 1 - i] == 2) rightDiagonalX++;
-        }
-
-        if (leftDiagonalO == N || rightDiagonalO == N) return 'O';
-        if (leftDiagonalX == N || rightDiagonalX == N) return 'X';
-
-        return hasEmptyCell ? '?' : '-';
-    }
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        int N = scanner.nextInt();
-        int[][] board = new int[N][N];
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                board[i][j] = scanner.nextInt();
+        int ukuranPapan = input.nextInt();
+        int[][] papan = new int[ukuranPapan][ukuranPapan];
+        
+      
+        boolean adaTempatKosong = false;
+        for (int i = 0; i < ukuranPapan; i++) {
+            for (int j = 0; j < ukuranPapan; j++) {
+                papan[i][j] = input.nextInt();
+                if (papan[i][j] == 0) {
+                    adaTempatKosong = true;
+                }
             }
         }
 
-        char winner = checkWinner(board, N);
-        System.out.println(winner);
+        int pemenang = 0; 
 
-        scanner.close();
-   }
+      
+        for (int i = 0; i < ukuranPapan; i++) {
+            
+            boolean barisSama = true;
+            int nilaiBaris = papan[i][0];
+            if (nilaiBaris != 0) {
+                for (int j = 1; j < ukuranPapan; j++) {
+                    if (papan[i][j] != nilaiBaris) {
+                        barisSama = false;
+                        break;
+                    }
+                }
+                if (barisSama) pemenang = nilaiBaris;
+            }
+
+           
+            boolean kolomSama = true;
+            int nilaiKolom = papan[0][i];
+            if (nilaiKolom != 0) {
+                for (int j = 1; j < ukuranPapan; j++) {
+                    if (papan[j][i] != nilaiKolom) {
+                        kolomSama = false;
+                        break;
+                    }
+                }
+                if (kolomSama) pemenang = nilaiKolom;
+            }
+        }
+
+       
+        boolean diagUtamaSama = true;
+        int nilaiDiagUtama = papan[0][0];
+        if (nilaiDiagUtama != 0) {
+            for (int i = 1; i < ukuranPapan; i++) {
+                if (papan[i][i] != nilaiDiagUtama) {
+                    diagUtamaSama = false;
+                    break;
+                }
+            }
+            if (diagUtamaSama) pemenang = nilaiDiagUtama;
+        }
+
+        
+        boolean diagSampingSama = true;
+        int nilaiDiagSamping = papan[0][ukuranPapan - 1];
+        if (nilaiDiagSamping != 0) {
+            for (int i = 1; i < ukuranPapan; i++) {
+                if (papan[i][ukuranPapan - 1 - i] != nilaiDiagSamping) {
+                    diagSampingSama = false;
+                    break;
+                }
+            }
+            if (diagSampingSama) pemenang = nilaiDiagSamping;
+        }
+
+       
+        if (pemenang == 1) {
+            System.out.println("O");
+        } else if (pemenang == 2) {
+            System.out.println("X");
+        } else if (adaTempatKosong) {
+            System.out.println("?");
+        } else {
+            System.out.println("-"); 
+        }
+        
+        input.close();
+    }
 }
